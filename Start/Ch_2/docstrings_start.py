e64d57295127955d4838f3931dafb5dc349163ed
Omer-Gamie <57861096+OmerGamie@users.noreply.github.com>
2023-08-23 12:43:02 +0000

    Modified Docstring

--- Start/Ch_2/docstrings_start.py
+++ Start/Ch_2/docstrings_start.py
@@ -2,8 +2,16 @@
 # Demonstrate the use of lambda functions
 
 
-def my_function(arg1, arg2=None):
-    print(arg1, arg2)
+def points(twopointers, threepointers):
+    """
+    points(twopointers, threepointers) -> Cout points for a basketball game,
+    it takes in the amount of 3-pointers scored and 2-pointers scored. It then
+    calculates the final points using the formula and returns that value.
+    Parameters:
+    twopointers: 2-pointers scored
+    threepointers: 3-pointers scored
+    """
+    return (twopointers * 2) + (threepointers * 3)
 
 
-print(my_function.__doc__)
+print(points.__doc__)
